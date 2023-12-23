f02ffa8d8681402a47813171da64c37ac8cd716b
Strivin0311 <hyp@smail.nju.edu.cn>
2023-12-24 02:23:15 +0800

    finished download_model pipeline without testing

--- README.md
+++ README.md
@@ -1,10 +1,59 @@
 # hf-downloader
 A repo to manage the downloaded resources from huggingface including model shards and dataset splits, with some scripts to add new resources, get meta info, view the resource structure, etc
 
+(*assuming your work-dir is `hf-downloader/` right now*)
 
-Note: 
-* we use the official tool [huggingface_hub](https://huggingface.co/docs/huggingface_hub/guides/download) to download the raw files from huggingface, which can be installed by pip as below:
+#### Preparation:
+
+* Creat the `.env` file to set some specific environment variables, which needs the python package `dotenv` installed by pip below:
+  ```sh
+  pip install -U python-dotenv
+  ```
+  
+* Set the root directory to put all the models and datasets in `.env` like:
+  ```sh
+  HF_MODEL_ROOT="/model/"
+  HF_DATASET_ROOT="/dataset/"
+  ```
+
+* Set the mirror address for huggingface model/dataset hub in `.env` like:
+  ```sh
+  HF_MIRROR="https://huggingface.co/"
+  ```
+
+* The official tool [huggingface_hub](https://huggingface.co/docs/huggingface_hub/guides/download) is adopted to download the raw files from huggingface, which can be installed by pip as below:
   ```sh
   pip install -U huggingface_hub
   ```
-* 
+* For faster download, we also use the tool [hf_transfer](https://huggingface.co/docs/huggingface_hub/v0.19.3/package_reference/environment_variables#hfhubenablehftransfer) to speed up file transfers, which can be installed by pip as below, with the only environment variable to set `.env`:
+  ```sh
+  pip install -U hf_transfer
+  HF_HUB_ENABLE_HF_TRANSFER=1
+  ```
+* To perminently store the resources, we've already kept the argument for the huggingface_hub functions `local_dir_use_symlinks=False` in the source code to disable the symlinks from the cache system
+
+#### Download model(s):
+
+* To download a model, you only have to:
+  * step1: search the [huggingface model hub](https://huggingface.co/models/) for the model name `source/my_model_name` you want
+  * step2: pick the directory `path/to/my_save_dir` you want to save it relative to the root directory `HF_MODEL_ROOT` set in the `.env` as an enviromental variable:
+  * step3: then simply run the command below, and you can wait until the model is successfully downloaded to the directory `HF_MODEL_ROOT/path/to/my_save_dir` and logged into `./log/model_downloaded.json`:
+    ```sh
+    python src/download_model.py --model_name `source/my_model_name` --save_dir `path/to/my_save_dir`
+    ```
+  * you can also refer to the more arguments usage in the demo scripts like `script/demo_download_xxx.sh`
+  * and the model config template can be found in `config/model_config_template.json`
+
+* To download a bunch of models sequentially in one go, you only have to:
+  * step1: encode the arguments you learned above into a dict-like config and append to the list in `./config/model_todownload.json` for each model you want to download
+  * step2: then simply run the command below, and you can patiently wait until the models are all successfully downloaded to the corresponding directories and logged into `./log/model_downloaded.json`: 
+    ```sh
+    python src/download_model.py --from_config
+    ```
+
+#### Download dataset(s):
+
+* To download a dataset:
+  ```sh
+  
+  ```
--- src/download_model.py
+++ src/download_model.py
@@ -1,13 +1,136 @@
 import os
 import argparse
 import time
+import json
+import traceback
 
-import transformers
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 
+model_root = os.getenv('HF_MODEL_ROOT')
+if model_root is None: raise ValueError("HF_MODEL_ROOT is not set")
+
+model_mirror = os.getenv('HF_MIRROR')
+if model_mirror is None: raise ValueError("HF_MIRROR is not set")
+
+model_config_template_path = './config/model_config_template.json'
+model_todownload_config_path = './config/model_todownload.json'
+model_downloaded_log_path = './log/model_downloaded.json'
+
+from huggingface_hub import hf_hub_download, snapshot_download
+
+## set config template dict
+config_template = {}
+with open(model_config_template_path, 'r', encoding='utf-8') as f: config_template = json.load(f)
+
+
+def check_config(config: dict):
+    if config.get('model_name', "") == "":
+        raise ValueError("model_name is required in the config fields")
+    if config.get('save_dir', "") == "":
+        raise ValueError("save_dir is required in the config fields")
+
+
+def downloaded_log(config):
+    # additional info to config
+    config['download_link'] = os.path.join(model_mirror, config['model_name'])
+    config['download_timestamp'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    
+    with open(model_downloaded_log_path, 'r', encoding='utf-8') as f:
+        downloaded_configs = json.load(f)
+    
+    if downloaded_configs == {}: # fist downloaded model
+        downloaded_configs = {'model_cnt': 0, 'models': []}
+    else:
+        downloaded_configs['model_cnt'] += 1
+        downloaded_configs['models'] = [config] + downloaded_configs['models']
+    
+    with open(model_downloaded_log_path, 'w', encoding='utf-8') as f:
+        json.dump(downloaded_configs, f)
+
+def download(config_dict: dict, idx=-1):
+    config = {**config_template, **config_dict}
+    check_config(config)
+    
+    orderstr_dict = {-1: " ", 0: " 1st ", 1: " 2nd ", 2: " 3rd "}
+    print("="*25, f" Downloading the{orderstr_dict.get(idx, ' '+str(idx)+'th ')}model: {config['model_name']} ", "="*25)
+    
+    # debug
+    # print(config)
+    # return
+    
+    try:
+        config['save_dir'] = os.path.join(model_root, config['save_dir'])
+        if not os.path.exists(config['save_dir']): os.makedirs(config['save_dir'])
+
+        if config['file_name'] != "": # to download specific files from the model repo
+            hf_hub_download(
+                repo_type='model',
+                repo_id=config['model_name'], 
+                filename=config['file_name'], 
+                revision=config['version'] if config['version'] != "" else None,
+                local_dir=config['save_dir'], 
+                local_dir_use_symlinks=False # NOTE: directly download the files instead of symlinks to the cache for perminent storage
+            )
+        else: # to download the whole model repo
+            snapshot_download(
+                repo_type='model',
+                repo_id=config['model_name'],
+                revision=config['version'] if config['version'] not in ["", "latest"] else None,
+                allow_patterns=config['allow_patterns'] if config['allow_patterns'] != [] else None,
+                ignore_patterns=config['ignore_patterns'] if config['ignore_patterns'] != [] else None,
+                local_dir=config['save_dir'], 
+                local_dir_use_symlinks=False # NOTE: directly download the files instead of symlinks to the cache for perminent storage
+            )
+            
+    except Exception as e:
+        print("="*25, f" Failed to Download: {config['model_name']} due to the thrown error\n: {e}\n", "="*25)
+        return False
+    
+    downloaded_log(config)
+    print("="*25, f" Successfully Downloaded: {config['model_name']} ", "="*25)
+    return True
+
+
+def main(args):
+    start_time = time.time()
+    todownload_cnt, downloaded_cnt = 1, 1
+    
+    if args.from_config: # download a bunch of models from the config file
+        with open(model_todownload_config_path, 'r', encoding='utf-8') as f:
+            configs = json.load(f)
+        if not isinstance(configs, list) or not isinstance(configs[0], dict): raise TypeError("the json-like config file should be a list of dict")
+
+        unsucceeded_configs, todownload_cnt = [], len(configs)
+        for idx, config in enumerate(configs):
+            succeeded = download(config, idx=idx)
+            if not succeeded: unsucceeded_configs.append(config)
+            
+        downloaded_cnt = todownload_cnt - len(unsucceeded_configs)
+        with open(model_todownload_config_path, 'w', encoding='utf-8') as f:
+            json.dump(unsucceeded_configs, f)
+    else: # download the single model configed by the arguments
+        download(vars(args))
+        
+    elapsed_time = time.time() - start_time
+    print("="*25, f" The model(s) has been downloaded: {downloaded_cnt} / {todownload_cnt}, within {time.strftime('%H:%M:%S', time.gmtime(elapsed_time))} seconds ", "="*25)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="The script to download a model from HuggingFace or its mirror sites.")
-    
+    parser.add_argument("--model_name", type=str, required=True, help="The model's name shown on the huggingface model page, like 'meta-llama/Llama-2-7b-chat-hf'")
+    parser.add_argument("--file_name", type=str, default="", help="The specific file name in the model repo, like 'config.json', default is empty to download the whole model repo")
+    parser.add_argument("--version", type=str, default="", help="The specific version of the model, like 'v1.0', default is empty for the latest version")
+    parser.add_argument("--save_dir", type=str, required=True, help=f"The directory to save the downloaded model repo or files, under the root dir: {model_root}")
+    parser.add_argument("--auth_token", type=str, default="", help="The auth token to download the model repo, default is empty to use the default auth token")
+    parser.add_argument("--allow_patterns", nargs='*', help="The wildcards-style patterns to allow downloading, default is empty list to allow everything, only used when downloading the entire repo, i.e. file_name=''")
+    parser.add_argument("--ignore_patterns", nargs='*', help="The wildcards-style patterns to ignore when downloading, default is empty list to allow everything, only used when downloading the entire repo, i.e. file_name=''")
+    parser.add_argument("--from_config", action="store_true", help=f"setting this True to ignore the args above, and directly use the json-like config file: {model_todownload_config_path} to download the models, "+ 
+                        "containing a list of dict-like configs, each of which gives the arguments for a model to download "+ 
+                        f"and would be cleaned and logged into {model_downloaded_log_path} if successfully downloaded"
+                        )
+
+    args = parser.parse_args()
+    main(args)
     
